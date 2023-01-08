098a5138f4c829d13d71a5530b806f2370321d6c
YaseminMZR <83133207+YaseminMZR@users.noreply.github.com>
2023-01-08 21:26:39 +0300

    Projede Düzenleme
    Jar dosyası oluşturulamadığı için proje silinip tekrar yüklendi.

--- OS23/src/dispatcher/ProcessSituation.java
+++
@@ -1,8 +0,0 @@
-package dispatcher;
-
-public enum ProcessSituation { //Proses statelerinin tanýmlandýðý enum
-	READY, //Hazýr
-	RUNNING, //Çalýþýyor
-	INTERRUPTED, //Kesilde(20 saniyeden fazla çalýþtýðý durumlar)
-	TERMINATED //Sonlandýrýldý
-}
